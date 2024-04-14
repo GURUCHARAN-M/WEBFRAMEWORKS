0c44d17b7c7c58ad4a9bf29af3295ed92bdde40d
Gurucharan M <124780867+GURUCHARAN-M@users.noreply.github.com>
2024-04-14 16:29:20 +0530

    Delete Day_5 directory

--- Day_5/q3/src/main/java/com/example/q3/repository/LanguageRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.q3.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.q3.model.Language;
-
-public interface LanguageRepo extends JpaRepository<Language,Integer> {
-    
-}
