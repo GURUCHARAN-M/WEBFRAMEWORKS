5dcaef8db336556cd3ed332b7a7f2ad59e236b22
Gurucharan M <124780867+GURUCHARAN-M@users.noreply.github.com>
2024-04-23 22:51:55 +0530

    day 10 updated

---
+++ Day_10/gurucharanq2/src/main/java/com/example/gurucharanq2/Gurucharanq2Application.java
@@ -0,0 +1,13 @@
+package com.example.gurucharanq2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gurucharanq2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gurucharanq2Application.class, args);
+	}
+
+}
